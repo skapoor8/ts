2a9cf747f3f450f200a62fb6506725a37e2ab9d1
Siddharth Kapoor <siddharth.kapoor24@gmail.com>
2022-11-17 09:24:07 -0500

    changed sql backup file name for windows...

--- node/databases/mikro-orm-mysql/scripts/export.sh
+++ node/databases/mikro-orm-mysql/scripts/export.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p src/backups
-mysqldump node_mikro_mysql_elist_manager > src/backups/$(date +%Y-%m-%d-%H:%M:%S).sql
+mysqldump node_mikro_mysql_elist_manager > src/backups/$(date +%Y-%m-%d-%H-%M-%S).sql

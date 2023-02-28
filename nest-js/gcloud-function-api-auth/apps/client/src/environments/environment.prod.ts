e034e5f5122b769239d1166b89c176e265854225
Siddharth Kapoor <siddharth.kapoor24@gmail.com>
2023-02-27 20:42:12 -0500

    removed files

--- nest-js/gcloud-function-api-auth/apps/client/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  apiEndpoint: 'https://func-looper-xbr2k3vaja-uc.a.run.app',
-  firebase: {
-    apiKey: 'AIzaSyBT2glrwdnRZecAtYskU4ckxNk62tRYjAU',
-    authDomain: 'looper-374421.firebaseapp.com',
-    projectId: 'looper-374421',
-  },
-};
--- nest-js/gcloud-function-api-auth/apps/client/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  apiEndpoint: 'http://localhost:8080',
-  firebase: {
-    apiKey: 'AIzaSyBT2glrwdnRZecAtYskU4ckxNk62tRYjAU',
-    authDomain: 'looper-374421.firebaseapp.com',
-    projectId: 'looper-374421',
-  },
-};
